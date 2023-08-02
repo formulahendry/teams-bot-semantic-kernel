c15fbb85036dfbf0f92eee6f1f2cf2559c2d8dbe
Jun Han <junhan@microsoft.com>
2023-08-02 12:18:10 +0800

    init

---
+++ Models/HelloWorldModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyTeamsAppSK_BotSSO.Models
+{
+    public class HelloWorldModel
+    {
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+    }
+}
---
+++ TeamsBot.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Teams;
+
+namespace MyTeamsAppSK_BotSSO
+{
+    /// <summary>
+    /// An empty bot handler.
+    /// You can add your customization code here to extend your bot logic if needed.
+    /// </summary>
+    public class TeamsBot : TeamsActivityHandler
+    {
+        public override Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default) =>
+            Task.CompletedTask;
+    }
+}
